69eab8d94315330aa575a373b34e731da5a1f200
v1centebarros <vmabarros@ua.pt>
2025-01-18 17:28:35 +0000

    remove item from the sidebar

--- frontend/lib/primitives.ts
+++ frontend/lib/primitives.ts
@@ -4,8 +4,6 @@ export const sidebar = {
     navMain: [{
         title: "Dashboard", url: "/dashboard", icon: LayoutDashboard, isActive: true
     }, {
-        title: "Documents", url: "/documents", icon: Library,
-    }, {
         title: "Search", url: "/search", icon: Search
     }, {
         title: "AI Chat", url: "/chat", icon: Sparkles
